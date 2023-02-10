d789aab4554014b344f9c94311fa581989e184ee
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-02-10 17:22:33 +0300

    Created Examples007_FindMax

---
+++ Examples007_FindMax/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 100;
+int b = 8;
+int c = 22;
+int d = 4;
+int e = 5;
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write (max);
+
