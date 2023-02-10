2413d148ce0dd6ebe252c3d615ecb3d25ad1d8d1
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-02-10 15:56:56 +0300

    Created Examples001_HelloWord

---
+++ Examples001_HelloWord/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("2 Hello, World!");
+Console.WriteLine("2 Hello, World!");
+Console.WriteLine("2 Hello, World!");
+Console.WriteLine("2 Hello, World!");
