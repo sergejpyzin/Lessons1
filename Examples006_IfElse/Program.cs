946e669af971f6289737a476a5ed1f2181e98d58
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-02-10 17:03:26 +0300

    Created Examples006_IfElse

---
+++ Examples006_IfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿string userName = Console.ReadLine();
+
+if (userName == "Маша") 
+{
+    Console.WriteLine ("ВАУ С НАМИ МАША");
+} 
+else
+{
+    Console.Write ("Привет ");
+    Console.WriteLine (userName);
+}
