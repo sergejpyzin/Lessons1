ef160c8c6fd2cf4cd4a4dd15c30bd3858553166c
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-02-10 16:25:49 +0300

    Created Examples005_FindSumRandom

---
+++ Examples005_FindSumRandom/Program.cs
@@ -0,0 +1,6 @@
+﻿int numberA = new Random().Next(0, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(0, 10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
